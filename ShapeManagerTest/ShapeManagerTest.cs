89fb59f0a2ab56ed0dca4d266eb2c7fe3897ed06
re-hiramatsu <re-hiramatsu>
2024-05-28 13:58:49 +0900

    ShapeManagerTestリファクタリング

--- ShapeManagerTest/ShapeManagerTest.cs
+++ ShapeManagerTest/ShapeManagerTest.cs
@@ -12,106 +12,52 @@ namespace ShapeManagerTest.Test
         ShapeManager shapeMana = new ShapeManager();
 
         /// <summary>
-        /// 正しくShapeListに三角形の面積が入っているか
+        /// 正しく三角形のGetShapeができているか
         /// </summary>
         [TestMethod]
-        public void TriangleAreaStoredCorrectlyInDobleShapeList()
+        public void TriangleGetShpaeTest()
         {
             Triangle triangle = new Triangle(3);
             shapeMana.AddShapeList(triangle);
             var shape = shapeMana.GetShape(0);
-            Assert.AreEqual(shape.GetArea(), 3.8971);
-        }
 
-        /// <summary>
-        /// 正しくShapeListに三角形の名前が入っているか
-        /// </summary>
-        [TestMethod]
-        public void TriangleNameStoredCorrectlyInShapeList()
-        {
-            Triangle triangle = new Triangle(3);
-            shapeMana.AddShapeList(triangle);
-            var shape = shapeMana.GetShape(0);
+            Assert.AreEqual(shape.GetArea(), 3.8971);
             Assert.AreEqual(shape.GetShapeName(), ShapeNameEnum.三角形);
         }
 
         /// <summary>
-        /// 正しくShapeListに四角形の面積が入っているか
+        /// 正しく四角形のGetShapeができているか
         /// </summary>
         [TestMethod]
-        public void QuadAreaStoredCorrectlyInShapeList()
-        {
-            Quadrilarea quad = new Quadrilarea(3);
-            shapeMana.AddShapeList(quad);
-            var shape = shapeMana.GetShape(0);
-            Assert.AreEqual(shape.GetArea(), 9);
-        }
-
-        /// <summary>
-        /// 正しくShapeListに四角形のdouble型の面積が入っているか
-        /// </summary>
-        [TestMethod]
-        public void QuadAreaStoredCorrectlyInDoubleShapeList()
+        public void QuadGetShapeTest()
         {
             Quadrilarea quad = new Quadrilarea(5.123);
             shapeMana.AddShapeList(quad);
             var shape = shapeMana.GetShape(0);
-            Assert.AreEqual(shape.GetArea(), 26.2451);
-        }
 
-        /// <summary>
-        /// 正しくShapeListに四角形の名前が入っているか
-        /// </summary>
-        [TestMethod]
-        public void QuadNameStoredCorrectlyInShapeList()
-        {
-            Quadrilarea quad = new Quadrilarea(3);
-            shapeMana.AddShapeList(quad);
-            var shape = shapeMana.GetShape(0);
+            Assert.AreEqual(shape.GetArea(), 26.2451);
             Assert.AreEqual(shape.GetShapeName(), ShapeNameEnum.四角形);
         }
 
         /// <summary>
-        /// 正しくShapeListに台形の面積が入っているか
+        /// 正しく台形のGetShapeができているか
         /// </summary>
         [TestMethod]
-        public void TrapezoidAreaStoredCorrectlyInShapeList()
+        public void TrapezoidGetShapeTest()
         {
-            Trapezoid trapeziod = new Trapezoid(2, 2, 2);
+            Trapezoid trapeziod = new Trapezoid(2.1, 2.1, 2.1);
             shapeMana.AddShapeList(trapeziod);
             var shape = shapeMana.GetShape(0);
-            Assert.AreEqual(shape.GetArea(), 4);
-        }
 
-        /// <summary>
-        /// 正しくShapeListに台形のdouble型の面積が入っているか
-        /// </summary>
-        [TestMethod]
-        public void TrapezoidAreaStoredCorrectlyInDoubleShapeList()
-        {
-            Trapezoid quad = new Trapezoid(2.1, 2.1, 2.1);
-            shapeMana.AddShapeList(quad);
-            var shape = shapeMana.GetShape(0);
             Assert.AreEqual(shape.GetArea(), 4.41);
-        }
-
-        /// <summary>
-        /// 正しくShapeListに台形の名前が入っているか
-        /// </summary>
-        [TestMethod]
-        public void TrapezoidNameStoredCorrectlyInShapeList()
-        {
-            Trapezoid trapeziod = new Trapezoid(1, 2, 3);
-            shapeMana.AddShapeList(trapeziod);
-            var shape = shapeMana.GetShape(0);
             Assert.AreEqual(shape.GetShapeName(), ShapeNameEnum.台形);
         }
 
         /// <summary>
-        /// 正しくShapeListに複数図形が入っているか
+        /// 正しくShapeListに複数図形が追加されるか
         /// </summary>
         [TestMethod]
-        public void ValidShapesAddedToShapeList()
+        public void ShapesAddTest()
         {
             Trapezoid trapeziod = new Trapezoid(1, 2, 3);
             Triangle triangle = new Triangle(3);
@@ -145,7 +91,7 @@ namespace ShapeManagerTest.Test
         ///正しくListがカウントされているか
         /// </summary>
         [TestMethod]
-        public void ListCountIsCorrectWithNoElements()
+        public void ListCountTest()
         {
             Trapezoid trapeziod = new Trapezoid(1, 2, 3);
             Triangle triangle = new Triangle(3);
@@ -164,7 +110,7 @@ namespace ShapeManagerTest.Test
         ///正しくListが削除されているか
         /// </summary>
         [TestMethod]
-        public void ElementIsCorrectlyRemovedFromList()
+        public void RemovedListTest()
         {
             Trapezoid trapeziod = new Trapezoid(1, 2, 3);
             Triangle triangle = new Triangle(3);
