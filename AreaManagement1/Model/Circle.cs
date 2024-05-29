fde1eac4b367ee452212c612c5de266a4b683fe2
re-hiramatsu <re-hiramatsu>
2024-05-29 13:31:13 +0900

    CircleTest実装

---
+++ AreaManagement1/Model/Circle.cs
@@ -0,0 +1,38 @@
+﻿using AreaManagement1;
+using System;
+
+namespace AreaManagement.Model
+{
+    public class Circle : Shape
+    {
+        private double _sideLength;
+
+        public double Length
+        {
+            get { return _sideLength; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception(Consts.MESSAGE_PLEASENUMVER);
+                }
+                _sideLength = value;
+            }
+        }
+
+        public Circle(double sideLength)
+        {
+            if (sideLength <= 0)
+            {
+                throw new Exception(Consts.MESSAGE_PLEASENUMVER);
+            }
+            _sideLength = sideLength;
+            _shapeName = ShapeNameEnum.円;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round(_sideLength * _sideLength * Math.PI, 4);
+        }
+    }
+}
---
+++ ShapeTest/CircleTest.cs
@@ -0,0 +1,95 @@
+﻿using AreaManagement;
+using AreaManagement.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ShapeTest
+{
+    [TestClass]
+    public class CircleTest
+    {
+        /// <summary>
+        /// 円のコンストラクタに0の値を入れた場合エラーメッセージが出るか
+        /// </summary>
+        [TestMethod]
+        public void ErrorForNegativeSideLengthInConstructor()
+        {
+            Assert.ThrowsException<Exception>(() => new Circle(0));
+            Assert.ThrowsException<Exception>(() => new Circle(-1));
+        }
+
+        /// <summary>
+        /// 円のSetLengthに0の値を入れた場合エラーメッセージが出るか
+        /// </summary>
+        [TestMethod]
+        public void ErrorForNegativeLengthInSetLength()
+        {
+            Circle circle = new Circle(1);
+
+            Assert.ThrowsException<Exception>(() => circle.Length = 0);
+            Assert.ThrowsException<Exception>(() => circle.Length = -1);
+        }
+
+        /// <summary>
+        /// 正しく円の名前を返しているか
+        /// </summary>
+        [TestMethod]
+        public void ReturnNameForValidCircle()
+        {
+            Circle circle = new Circle(5);
+
+            Assert.AreEqual(circle.GetShapeName(), ShapeNameEnum.円);
+        }
+
+        /// <summary>
+        /// 正しく円の面積が第４位で四捨五入されているか
+        /// </summary>
+        [TestMethod]
+        public void AreaRoundedDownTo4DecimalPlaces_ForValidCircle()
+        {
+            Circle circle = new Circle(3);
+            var result = Math.Round(3 * 3 * Math.PI, 4);
+
+            //四角形の面積は28.27433
+            Assert.AreEqual(circle.GetArea(), result);
+        }
+
+        /// <summary>
+        /// 正しく円の面積が第４位で四捨五入されているか
+        /// </summary>
+        [TestMethod]
+        public void AreaRoundedUpTo4DecimalPlaces_ForValidCircle()
+        {
+            Circle circle = new Circle(4);
+            var result = Math.Round(4 * 4 * Math.PI, 4);
+
+            //四角形の面積は50.26548
+            Assert.AreEqual(circle.GetArea(), result);
+        }
+
+        /// <summary>
+        /// 正しく円の面積を返しているか
+        /// </summary>
+        [TestMethod]
+        public void TriangleAreaWithValidSides_ReturnsCircleArea()
+        {
+            Circle circle = new Circle(5.0);
+            var result = Math.Round(5.0 * 5.0 * Math.PI, 4);
+
+            //円の面積は78.53981
+            Assert.AreEqual(circle.GetArea(), result);
+        }
+
+        /// <summary>
+        /// 正しく円の辺を返しているか
+        /// </summary>
+        [TestMethod]
+        public void ReturnEdgesForValidCircle()
+        {
+            Circle circle = new Circle(5);
+            var length = circle.Length;
+
+            Assert.AreEqual(length, 5);
+        }
+    }
+}
